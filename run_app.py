ebab2c4d1477e576215630e2fd7a290527e08f1e
Aditya Sapru <sapruads@gmail.com>
2025-06-09 19:38:51 +0530

    Reupload all files

--- run_app.py
+++
@@ -1,9 +0,0 @@
-import os
-from dotenv import load_dotenv
-
-# Load environment variables from .env file
-load_dotenv()
-
-# Run the streamlit app
-if __name__ == "__main__":
-    os.system("streamlit run app.py")
